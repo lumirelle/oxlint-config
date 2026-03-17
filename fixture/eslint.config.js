728660f2ca9da140bc4bef3c87e95ec27f39bac8
Lumirelle <shabbyacc@outlook.com>
2026-03-17 23:05:47 +0800

    chore: update config template & scripts

---
+++ fixture/eslint.config.js
@@ -0,0 +1,32 @@
+// @ts-check
+import { antfu } from '@antfu/eslint-config'
+
+export default antfu(
+  {
+    type: 'lib',
+  },
+).override('antfu/perfectionist/setup', {
+  rules: {
+    'perfectionist/sort-imports': [
+      'error',
+      {
+        environment: 'bun',
+        groups: [
+          'type-import',
+          ['type-parent', 'type-sibling', 'type-index', 'type-internal'],
+          'value-builtin',
+          'value-external',
+          'value-internal',
+          ['value-parent', 'value-sibling', 'value-index'],
+          'side-effect',
+          'ts-equals-import',
+          'unknown',
+        ],
+        newlinesBetween: 'ignore',
+        newlinesInside: 'ignore',
+        order: 'asc',
+        type: 'natural',
+      },
+    ],
+  },
+})
