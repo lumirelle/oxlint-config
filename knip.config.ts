579fcbc5094e04cfa5b742607c8ecfa5d653957e
Lumirelle <shabbyacc@outlook.com>
2026-05-09 00:11:59 +0800

    chore: re-organize deps & update tsconfig & knip config

--- knip.config.ts
+++ knip.config.ts
@@ -1,8 +1,6 @@
 import type { KnipConfig } from 'knip'
 
 export default {
-  /// keep-sorted
   ignoreDependencies: ['@arethetypeswrong/cli', 'publint', 'vue'],
-  /// keep-sorted
   ignoreFiles: ['fixtures/**', 'test/fixtures/**'],
 } satisfies KnipConfig
