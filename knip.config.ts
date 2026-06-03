8f09c243b2b272624447205a0faf397c35959798
Lumirelle <shabbyacc@outlook.com>
2026-06-03 11:32:20 +0800

    chore: update knip config

--- knip.config.ts
+++ knip.config.ts
@@ -3,5 +3,5 @@ import type { KnipConfig } from 'knip'
 export default {
   entry: ['{src,test,fixtures,scripts}/**/*.{ts,js,vue}'],
   ignoreBinaries: ['mise'],
-  ignoreDependencies: ['@arethetypeswrong/cli', 'publint', 'bumpp'],
+  ignoreDependencies: ['@arethetypeswrong/cli', 'nano-staged', 'publint', 'bumpp'],
 } satisfies KnipConfig
