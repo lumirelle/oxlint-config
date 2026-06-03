49b7966aeb0f1e313fb4dcbb8e84cad937c737f7
Lumirelle <shabbyacc@outlook.com>
2026-06-03 10:49:24 +0800

    feat: update to oxlint 1.68.0 & oxlint-tsgolint 0.23.0

--- src/rules/typescript.ts
+++ src/rules/typescript.ts
@@ -3,7 +3,6 @@ import { typescript as _typescript, renamePluginInConfigs } from '@antfu/eslint-
 import { getConfig } from '../utils'
 
 const TS_RULES_NO_INTEND_SUPPORT_LIST = ['no-dupe-args', 'no-new-symbol']
-const TS_RULES_CURRENTLY_NOT_SUPPORTED_LIST = ['typescript/method-signature-style']
 
 export async function typescript(): Promise<OxlintConfig> {
   const configItems = renamePluginInConfigs(await _typescript(), { ts: 'typescript' })
@@ -12,10 +11,6 @@ export async function typescript(): Promise<OxlintConfig> {
     if (TS_RULES_NO_INTEND_SUPPORT_LIST.includes(key))
       // No intend to support by oxlint, see
       // https://github.com/oxc-project/oxc/issues/479
-      delete rules[key]
-    if (TS_RULES_CURRENTLY_NOT_SUPPORTED_LIST.includes(key))
-      // Currently does not supported by oxlint, see
-      // https://github.com/oxc-project/oxc/issues/2180
       delete rules[key]
   }
   return {
