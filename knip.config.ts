bb1965f3702aa9a98f1bf90ed36349b296c54adb
Lumirelle <shabbyacc@outlook.com>
2026-06-07 15:04:44 +0800

    build: upgrade to workflow v8, use mise tasks to perform releasing

--- knip.config.ts
+++ knip.config.ts
@@ -2,6 +2,4 @@ import type { KnipConfig } from 'knip'
 
 export default {
   entry: ['{src,test,fixtures,scripts}/**/*.{ts,js,vue}'],
-  ignoreBinaries: ['mise'],
-  ignoreDependencies: ['nano-staged'],
 } satisfies KnipConfig
