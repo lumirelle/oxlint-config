4f88a9ceae569b9b573a35c0215b18492426f381
Lumirelle <shabbyacc@outlook.com>
2026-06-16 14:37:23 +0800

    feat!: update to oxlint 1.70.0

--- knip.config.ts
+++ knip.config.ts
@@ -3,4 +3,5 @@ import type { KnipConfig } from 'knip'
 export default {
   entry: ['{src,test,fixtures,scripts}/**/*.{ts,js,vue}'],
   ignoreFiles: ['taze.config.ts'],
+  ignoreDependencies: ['taze'],
 } satisfies KnipConfig
